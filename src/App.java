a7d927123a8c8f9dcf5f0a609b264af0bdd9ae90
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-04-08 19:56:53 -0300

    resolução de problema na entrada de texto

--- src/App.java
+++ src/App.java
@@ -2,23 +2,31 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // 1- Imprima a informação do provedor
         String provedor = "CineMax";
         System.out.printf("Olá, bem-vindo a central de atendimento %s", provedor);
+  
+        // 2- Imprima a informação do assistente
         String atendente = "Mary Jane";
         System.out.printf("\nMeu nome é %s, sou seu assistente virtual", atendente);
 
+        // 3- Imprima a informação das instruções
         System.out.print("\nDurante o atendimento siga minhas instruções");
 
-        System.out.print("\nPrimeiramente informe seu nome");
+        // 4- Leia o nome da pessoa
         Scanner scanner = new Scanner(System.in);
-        String nome = scanner.next();
+        System.out.print("\nPrimeiramente informe seu nome");
+        String nome = scanner.nextLine();
 
+        // 5- Leia a pergunta da pessoa
         System.out.printf("Obrigado %s, como posso lhe ajudar?", nome);
+        String pergunta = scanner.nextLine();
 
-        String resposta = scanner.next();
+        // Imprimindo texto concatenado
+        // 6- Imprima a pergunta da pessoa
+        System.out.println("Sua pergunta foi: " + pergunta);
 
-        System.out.printf("\nSua resposta foi: \n %s", resposta);
-
+        // 7 - Feche o scanner
         scanner.close();
     }
 }
